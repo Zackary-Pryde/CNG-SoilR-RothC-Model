b667cae1a0f836ed305eb16ccd810d0cce78aa85
Zackary Pryde <zackary.pryde@anthesisgroup.com>
2023-10-12 15:04:48 +0200

    Updated script to run simulation function for testing

--- SoilR_Simulation_Testing_2.R
+++ SoilR_Simulation_Testing_2.R
@@ -53,50 +53,53 @@ BIOptf = 1.8943
 HUMptf = 72.5269 
 FallIOM = 9.0260
 
-years = seq(1/12,500,by=1/12) 
+years = seq(1/12,3,by=1/12) 
 
 # ROTH C MODEL FUNCTION .
-
-#Temperature factor per month
-fT=fT.RothC(Temp[,2])
-
-#Moisture effects per month . 
-fw1func<-function(P, E, S.Thick = 30, pClay = 32.0213, pE = 1, bare) {
-  M = P - E * pE
-  Acc.TSMD = NULL
-  for (i in 2:length(M)) {
-    B = ifelse(bare[i] == FALSE, 1, 1.8)
-    Max.TSMD = -(20 + 1.3 * pClay - 0.01 * (pClay^2)) * (S.Thick/23) * (1/B)
-    Acc.TSMD[1] = ifelse(M[1] > 0, 0, M[1])
-    if (Acc.TSMD[i - 1] + M[i] < 0) {
-      Acc.TSMD[i] = Acc.TSMD[i - 1] + M[i]
+Roth_C<-function(Cinputs,years,
+                 DPMptf, RPMptf, BIOptf, HUMptf, FallIOM,
+                 Temp,Precip,Evp,Cov2,soil.thick,SOC,clay,DR,bare1){
+  #Temperature factor per month
+  fT=fT.RothC(Temp[,2])
+  
+  #Moisture effects per month . 
+  fw1func<-function(P, E, S.Thick = 30, pClay = 32.0213, pE = 1, bare) {
+    M = P - E * pE
+    Acc.TSMD = NULL
+    for (i in 2:length(M)) {
+      B = ifelse(bare[i] == FALSE, 1, 1.8)
+      Max.TSMD = -(20 + 1.3 * pClay - 0.01 * (pClay^2)) * (S.Thick/23) * (1/B)
+      Acc.TSMD[1] = ifelse(M[1] > 0, 0, M[1])
+      if (Acc.TSMD[i - 1] + M[i] < 0) {
+        Acc.TSMD[i] = Acc.TSMD[i - 1] + M[i]
+        } else 
+          (Acc.TSMD[i] = 0)
+      if (Acc.TSMD[i] <= Max.TSMD) {
+        Acc.TSMD[i] = Max.TSMD
+      }
+      }
+    b = ifelse(Acc.TSMD > 0.444 * Max.TSMD, 1, (0.2 + 0.8 * ((Max.TSMD - Acc.TSMD)/(Max.TSMD - 0.444 * Max.TSMD))))
+    b<-clamp(b,lower=0.2)
+    return(data.frame(Acc.TSMD, b, Max.TSMD))
     }
-    else (Acc.TSMD[i] = 0)
-    if (Acc.TSMD[i] <= Max.TSMD) {
-      Acc.TSMD[i] = Max.TSMD
-    }
+  
+  fW_2<- fw1func(P=(Precip[,2]), E=(Evp[,2]), S.Thick = soil.thick, pClay = clay, pE = 1, bare=bare1$Soil_Cover)$b
+  
+  #Vegetation Cover effects 
+  fC <- Cov2[,2]
+  
+  # Set the factors frame for Model calculations
+  xi.frame=data.frame(years,rep(fT*fW_2*fC*fPR,length.out=length(years)))
+  
+  # RUN THE MODEL FROM SOILR
+  Model3_spin=RothCModel(t=years,C0=c(DPMptf, RPMptf, BIOptf, HUMptf, FallIOM),In=Cinputs,DR=DR,clay=clay,xi=xi.frame, pass=TRUE) 
+  Ct3_spin=getC(Model3_spin)
+  
+  # Get the final pools of the time series
+  poolSize3_spin=as.numeric(tail(Ct3_spin,1))
+  return(poolSize3_spin)
   }
-  b = ifelse(Acc.TSMD > 0.444 * Max.TSMD, 1, (0.2 + 0.8 * ((Max.TSMD - Acc.TSMD)/(Max.TSMD - 0.444 * Max.TSMD))))
-  b<-clamp(b,lower=0.2)
-  return(data.frame(Acc.TSMD, b, Max.TSMD))
-}
 
-fW_2<- fw1func(P=(Precip[,2]), E=(Evp[,2]), S.Thick = soil.thick, pClay = clay, pE = 1, bare=bc$Soil_Cover)$b
-
-#Vegetation Cover effects 
-fC <- Cov2[,2]
-
-# Set the factors frame for Model calculations
-xi.frame=data.frame(years,rep(fT*fW_2*fC*fPR,length.out=length(years)))
-
-# RUN THE MODEL FROM SOILR
-Model3_spin=RothCModel(t=years,C0=c(DPMptf, RPMptf, BIOptf, HUMptf, FallIOM),In=Cinputs,DR=DR,clay=clay,xi=xi.frame, pass=TRUE) 
-Ct3_spin=getC(Model3_spin)
-
-# Get the final pools of the time series
-poolSize3_spin=as.numeric(tail(Ct3_spin,1))
-
-poolSize3_spin
 SOC_BL = sum(poolSize3_spin)
 
 
