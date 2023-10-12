e3efccc7b33f9837700ba57c244ffd86350b9375
Zackary Pryde <zackary.pryde@anthesisgroup.com>
2023-10-12 13:00:13 +0200

    Random Edits

--- SoilR_Simulation_Testing_2.R
+++ SoilR_Simulation_Testing_2.R
@@ -4,6 +4,12 @@
 # 
 # This R script is the first attempt at RothC model SIMULATION using SoilR.
 # The code below follows the code set out at the following resource:
-# 
+# https://fao-gsp.github.io/GSOCseq/software-environment.html
 # 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+# 1. Required Packages/Dependencies ----
+
+library(pacman)
+p_load(raster, rgdal, ncdf4, SoilR, abind, soilassessment, Formula)
+
