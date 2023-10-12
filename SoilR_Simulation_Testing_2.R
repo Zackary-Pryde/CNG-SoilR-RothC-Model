9f53fab14ab8c114843a4295613362bdcfb185c8
Jackie Raw <jacqueline.raw@anthesisgroup.com>
2023-10-12 11:14:49 +0200

    ,,

---
+++ SoilR_Simulation_Testing_2.R
@@ -0,0 +1,9 @@
+# 0. File Information ----
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# 
+# This R script is the first attempt at RothC model SIMULATION using SoilR.
+# The code below follows the code set out at the following resource:
+# 
+# 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
