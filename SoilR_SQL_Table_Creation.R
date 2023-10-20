2d213c98c3cfec042a889ef31b7fe8c22a7226b1
Zackary Pryde <zackary.pryde@anthesisgroup.com>
2023-10-20 14:22:23 +0200

    SQL Table creation script for input data and password protected credentials to access the database

--- SoilR_SQL_Table_Creation.R
+++ SoilR_SQL_Table_Creation.R
@@ -15,4 +15,36 @@
 # 1. Required Packages/Dependencies ----
 
 library(pacman)
-p_load(raster, rgdal, ncdf4, SoilR, abind, soilassessment, Formula)
+p_load(odbc,tidyverse,RODBC,DBI,dplyr, keyring)
+
+# 2. Connecting to the SQL database securely ----
+# 
+# We need to form a SQL database connection without sharing credentials within the script.
+# Two methods may be applicable here:
+#   1. Use a secrets keyring
+#   2. Use interactive prompts
+
+# KEYRING METHOD
+# 
+# NB Keyring has been created locally and is password protected. 
+
+keyring_unlock("cng_SQL_Credentials")
+connection <- odbc::dbConnect(odbc(),Driver = "ODBC Driver 18 for SQL Server",
+                              Server = key_get("SQL_Server", keyring ="cng_SQL_Credentials"),
+                              Database = key_get("Database", keyring ="cng_SQL_Credentials"),
+                              UID = key_get("Username", keyring ="cng_SQL_Credentials"),
+                              PWD = key_get("Password", keyring ="cng_SQL_Credentials"))
+keyring_lock("cng_SQL_Credentials")
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# INTERACTIVE PROMPTING
+# connection <- odbc::dbConnect(odbc(),Driver = "ODBC Driver 18 for SQL Server",
+#                               Server = rstudioapi::showPrompt(title = "SQL Server", message = "Specify Server", default = ""),
+#                               Database = rstudioapi::showPrompt(title = "Database", message = "Specify Database", default = ""),
+#                               UID = rstudioapi::showPrompt(title = "Username", message = "Enter Username", default = ""),
+#                               PWD = rstudioapi::showPrompt(title = "Password", message = "Enter Password", default = ""))
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# 3. Create SQL tables for SoilR Input Data
